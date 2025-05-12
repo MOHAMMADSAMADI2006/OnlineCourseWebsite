bdcbc456212e53a3081635e0032e4c7360a5390c
Abolfazli78 <ghoranman@gmail.com>
2025-05-12 14:30:05 +0330

    Cqrs

---
+++ Domain/Interface/IReadRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interface
+{
+    public interface IReadRepository<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<IEnumerable<T>> ListAsync(Expression<Func<T, bool>> predicate = null);
+      
+    }
+}
---
+++ Infrastructure/Persistence/Repositories/GenericWriteRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Interface;
+
+namespace Infrastructure.Persistence.Repositories
+{
+    //public class GenericWriteRepository : IWriteRepository<T>
+    //{
+    //    public Task AddAsync(T entity)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+
+    //    public void Remove(T entity)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+
+    //    public Task<int> SaveChangesAsync()
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+
+    //    public void Update(T entity)
+    //    {
+    //        throw new NotImplementedException();
+    //    }
+    //}
+}
